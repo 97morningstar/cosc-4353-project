55dbd31b17dc884e1ce872de2b215dded9f36cf3
Elisa Martinez <97morningstar@gmail.com>
2021-11-30 19:49:42 -0600

    Tests Added

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,18 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Header from './components/layout/Header';
 import Home from './components/Home';
 
-import LogIn from './components/LogIn';
-
-
-import firebase from 'firebase/compat/app';
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  onAuthStateChanged, 
-  signInWithEmailAndPassword,
-  signOut } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"
-
 test('renders Header component', () => {
   render(<Header />);
   const linkElement = screen.getByText(/Sign up/i);
