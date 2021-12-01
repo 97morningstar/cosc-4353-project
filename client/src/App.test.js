da820c892418bd56cee64a4312aca852ac6bd806
Elisa Martinez <97morningstar@gmail.com>
2021-11-30 20:52:29 -0600

    Testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,16 @@ import { render, screen } from '@testing-library/react';
 import Header from './components/layout/Header';
 import Home from './components/Home';
 
+import firebase from 'firebase/compat/app';
+
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signInWithEmailAndPassword,
+  signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
 test('renders Header component', () => {
   render(<Header />);
   const linkElement = screen.getByText(/Sign up/i);
@@ -15,4 +25,29 @@ test('renders Header component', () => {
 });
 
 
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
+
+app.auth = () => {
+  return { currentUser: { uid: "alice", email: "alice@example.com" } };
+}
+
+const auth = () => {
+  return { currentUser: { uid: null } };
+}
+jest.spyOn(app, 'auth').mockImplementation(auth);
+
+const login = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+test('Login', () => {
+ const info = login('martinezfuenteselisa123321@gmail.com', '123456')
+});
 
